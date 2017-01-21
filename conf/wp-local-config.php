7be28bbe1abd391029b96d39457fea52e8823a81
chriszarate <chris@zarate.org>
2017-01-21 11:13:02 -0500

    Remove unnecessary object cache flag.

--- conf/wp-local-config.php
+++ conf/wp-local-config.php
@@ -8,9 +8,7 @@
 // This should match the container link name defined in `docker-compose.yml`.
 define( 'DB_HOST', 'mysql' );
 
-// We don't provide memcached or asset minification. Changing these values will
-// likely result in a fatal error.
-define( 'WP_OBJECT_CACHE', false );
+// Disable asset minification.
 define( 'QUICKSTART_DISABLE_CONCAT', true );
 
 // Project-specific config.
